2ee39b405faed9d25aadc7db03d247581c2ffd06
Tracy <tracycobrien@gmail.com>
2019-04-30 10:18:18 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Sheldon",
+		photo: "https://upload.wikimedia.org/wikipedia/en/f/f7/Sheldon_and_Young_Sheldon.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Penny",
+		photo: "https://en.wikipedia.org/wiki/Penny_(The_Big_Bang_Theory)#/media/File:Penny_bigbangtheory.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Leonard",
+		photo: "https://en.wikipedia.org/wiki/Leonard_Hofstadter#/media/File:Leonard_Hofstadter.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Bernadette",
+		photo: "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_and_Young_Sheldon_characters#/media/File:Melissa_Rauch_at_PaleyFest_2013_(edited).jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Raj",
+		photo: "https://en.wikipedia.org/wiki/Raj_Koothrappali#/media/File:Rajesh_Koothrappali.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
